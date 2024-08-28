2de608d304d9e6818afba1425b000236d62d7ec5
nic11371 <nic11371@gmail.com>
2024-08-28 23:03:28 +0300

    nes

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,7 @@ from gendiff.module.gendiff import generate_diff as gendiff
 
 def main():
     # parser = argparse.ArgumentParser(
-    #     description='Compares two configuration files and shows a difference.')
+    #   description='Compares two configuration files and shows a difference.')
     # parser.add_argument('first_file')
     # parser.add_argument('second_file')
     # parser.add_argument("-f", "--format", help="set format of output")
