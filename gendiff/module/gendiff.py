a3c8d1a7ddf626b45a949afde76b60a8842681cc
Nic11371 <nic11371@gmail.com>
2024-09-01 15:49:53 +0300

    step_3

--- gendiff/module/gendiff.py
+++ gendiff/module/gendiff.py
@@ -1,30 +1,20 @@
 import json
+import itertools
 
 
 def generate_diff(file_path1, file_path2):
     dict1 = json.load(open(file_path1))
     dict2 = json.load(open(file_path2))
-    string = ''
-    for key1 in dict1.keys():
-        values1 = dict1[key1]
-        if key1 in dict2:
-            if values1 == dict2[key1]:
-                string += f"{key1}: {values1}, "
-            string += f"{key1}: {dict2[key1]}, "
-            string += f"{key1}: {values1}"
-    print(string)
-    # add = {key: value for key, value in dict2.items() - dict1.items()}
-    # remove = {key: value for key, value in dict1.items() - dict2.items()}
-    # union = {key: value for key, value in dict1.items() & dict2.items()}
 
-    # diff = add | remove | union
+    add = [f"+ {key}: {str(value).lower()}"
+           for key, value in dict2.items() - dict1.items()]
+    remove = [f"- {key}: {str(value).lower()}"
+              for key, value in dict1.items() - dict2.items()]
+    union = [f"  {key}: {str(value).lower()}"
+             for key, value in dict2.items() & dict1.items()]
+    result = remove + add + union
+    f = list(itertools.chain('{', sorted(result, key=lambda s: s[2]), '}'))
+    return '\n'.join(f)
 
-    # string = str(map(f"\n{key}: {value}", add))
-    # for items in dict1.keys() | dict2.keys():
-    #     key, value = 
-    #     string += f'''\n{div * count * depth}{div * count}{key}: {value}'''
-    #     result = list(
-    #         itertools.chain("{", string, "\n", div * depth, "}"))
-    # return ''.join(result)
 
-    # print(diff)
+print(generate_diff('gendiff/path/file1.json', 'gendiff/path/file2.json'))
