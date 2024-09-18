eb4aa168a3eff6d65d377a12639d2d45cff1058f
Nic11371 <nic11371@gmail.com>
2024-09-18 22:39:36 +0300

    book

--- gendiff/formatter.py
+++ gendiff/formatter.py
@@ -1,10 +1,19 @@
 import itertools
 
 
+def to_string(value):
+    if isinstance(value, dict):
+        list_ = []
+        for key, item in value.items():
+            list_.append(f"{key}: {to_string(item)}")
+        return "\n".join(list_)
+    return f"{value}"
+
+
 def stylish(diff):
     list_dict = []
     for key, value in diff.items():
-        if isinstance(key, dict):
-            list_dict.append(f"{key}: {stylish(value)}")
+        if key == 'children':
+            list_dict.append(stylish(value))
     return list_dict
     
