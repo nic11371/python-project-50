87065d378fe81f202e36ba2a3d6188347e37d6ae
Nic11371 <nic11371@gmail.com>
2024-09-23 22:33:54 +0300

    formatter_beta2

--- gendiff/formatter.py
+++ gendiff/formatter.py
@@ -1,33 +1,42 @@
+import itertools
+
+
+OLD = '+'
+NEW = '-'
+UNCHARGED = ' '
+
+
 def stylish(diff):
     lists = []
     children = diff.get('children')
-    if children:
-        for elem in children:
-            key = elem.get('name')
-            type = elem.get('type')
-            if type == 'add':
-                lists.append(f"{key}: {string(elem.get('value'))}")
-            if type == 'remove':
-                lists.append(f"{key}: {string(elem.get('value'))}")
-            if type == 'uncharged':
-                lists.append(f"{key}: {string(elem.get('value'))}")
-            if type == 'nested':
-                lists.append(f"{key}: {stylish(elem)}")
-            if type == 'modified':
-                lists.append(f"{key}: {string(elem.get('old_value'))}")
-                lists.append(f"{key}: {string(elem.get('new_value'))}")
-    return "\n".join(lists)
+    for elem in children:
+        key = elem.get('name')
+        type = elem.get('type')
+        value = elem.get('value')
+        if type == 'add':
+            lists.append(f"{key}: {to_string(value)}")
+        if type == 'remove':
+            lists.append(f"{key}: {to_string(value)}")
+        if type == 'uncharged':
+            lists.append(f"{key}: {to_string(value)}")
+        if type == 'nested':
+            lists.append(f"{key}: {stylish(elem)}")
+        if type == 'modified':
+            lists.append(f"{key}: {to_string(elem.get('old_value'))}")
+            lists.append(f"{key}: {to_string(elem.get('new_value'))}")
+    format = "\n".join(lists)
+    return f"{{\n{format}\n}}"
 
 
-def string(item):
+def to_string(item):
     lists = []
     if item is None:
         return 'null'
     if isinstance(item, bool):
         return str(item).lower()
     if isinstance(item, dict):
-        for k, val in item.items():
-            format = string(val)
-            lists.append(f"{k}: {format}")
+        for key, val in item.items():
+            format = to_string(val)
+            lists.append(f"{'{'}\n{key}: {format}\n{'}'}")
         return "\n".join(lists)
     return item
