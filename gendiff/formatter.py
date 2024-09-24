4c2ed6802f0dba01d1e1cb053563cb224a303719
Nic11371 <nic11371@gmail.com>
2024-09-24 21:46:02 +0300

    gendiff_beta3

--- gendiff/formatter.py
+++ gendiff/formatter.py
@@ -1,12 +1,12 @@
-import itertools
+OLD = '- '
+NEW = '+ '
+UNCHARGED = '  '
+SEPARATOR = ' '
 
 
-OLD = '+'
-NEW = '-'
-UNCHARGED = ' '
 
-
-def stylish(diff):
+def stylish(diff, depth=2):
+    space = depth * SEPARATOR
     lists = []
     children = diff.get('children')
     for elem in children:
@@ -14,29 +14,34 @@ def stylish(diff):
         type = elem.get('type')
         value = elem.get('value')
         if type == 'add':
-            lists.append(f"{key}: {to_string(value)}")
+            lists.append(f"{space}{NEW}{key}: {to_string(value, depth)}")
         if type == 'remove':
-            lists.append(f"{key}: {to_string(value)}")
+            lists.append(f"{space}{OLD}{key}: {to_string(value, depth)}")
         if type == 'uncharged':
-            lists.append(f"{key}: {to_string(value)}")
+            lists.append(f"{space}{UNCHARGED}{key}: {to_string(value, depth)}")
         if type == 'nested':
-            lists.append(f"{key}: {stylish(elem)}")
+            lists.append(f"{space}{UNCHARGED}{key}: {stylish(elem, depth + 4)}")
         if type == 'modified':
-            lists.append(f"{key}: {to_string(elem.get('old_value'))}")
-            lists.append(f"{key}: {to_string(elem.get('new_value'))}")
+            lists.append(f"{space}{OLD}{key}: {to_string(elem.get('old_value'), depth)}")
+            lists.append(f"{space}{NEW}{key}: {to_string(elem.get('new_value'), depth)}")
     format = "\n".join(lists)
-    return f"{{\n{format}\n}}"
+    end_space = SEPARATOR * (depth - 2)
+
+    return f"{{\n{format}\n{end_space}}}"
 
 
-def to_string(item):
+def to_string(item, depth=2):
     lists = []
     if item is None:
         return 'null'
     if isinstance(item, bool):
         return str(item).lower()
     if isinstance(item, dict):
+        space = SEPARATOR * (depth + 4)
         for key, val in item.items():
-            format = to_string(val)
-            lists.append(f"{'{'}\n{key}: {format}\n{'}'}")
-        return "\n".join(lists)
-    return item
+            format = to_string(val, depth + 4)
+            lists.append(f"{space}{UNCHARGED}{key}: {format}")
+        format = "\n".join(lists)
+        end_space = SEPARATOR * (depth + 2)
+        return f"{{\n{format}\n{end_space}}}"
+    return f"{item}"
