cae0d816baa939cc42ddd7fb21bb14fe9a50067d
Nic11371 <nic11371@gmail.com>
2024-09-29 22:14:20 +0300

    refactor_1.2

--- tests/test_diff_formats.py
+++ tests/test_diff_formats.py
@@ -1,5 +1,5 @@
 import pytest
-from tests._test_format import _test_format
+from tests.format import format_diff
 
 
 @pytest.mark.parametrize('file1_name, file2_name, out, format', [
@@ -9,8 +9,6 @@ from tests._test_format import _test_format
     ('file1.json', 'file3.yaml', 'except_result_json.txt', 'json'),
     ('file1.yml', 'file3.yaml', 'except_result_json.txt', 'json'),
 ])
-
-
 def test_json(file1_name, file2_name, out, format):
-    actual_result, expected_result = _test_format(file1_name, file2_name, out, format)
-    assert not actual_result == expected_result
+    actual, expected = format_diff(file1_name, file2_name, out, format)
+    assert not actual == expected
--- tests/test_json.py
+++ tests/test_json.py
@@ -1,5 +1,5 @@
 import pytest
-from tests._test_format import _test_format
+from tests.format import format_diff
 
 
 @pytest.mark.parametrize('file1_name, file2_name, out, format', [
@@ -10,8 +10,6 @@ from tests._test_format import _test_format
     ('file1.yaml', 'file3.yaml', 'except_result_json.txt', 'json'),
     ('file1.yml', 'file3.yml', 'except_result_json.txt', 'json'),
 ])
-
-
 def test_json(file1_name, file2_name, out, format):
-    actual_result, expected_result = _test_format(file1_name, file2_name, out, format)
-    assert actual_result == expected_result
+    actual, expected = format_diff(file1_name, file2_name, out, format)
+    assert actual == expected
